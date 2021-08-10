d084350f987916a688d7f13b9f730142876dcfa1
woorim1022 <woorim1022@naver.com>
2021-08-10 14:04:32 +0900

    Modify BOJ1158_요세푸스문제.java

--- swexpertacademy/problems/src/com/woorim/problems/BOJ1158_요세푸스문제.java
+++ swexpertacademy/problems/src/com/woorim/problems/BOJ1158_요세푸스문제.java
@@ -6,40 +6,35 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class BOJ1158_요세푸스문제 {
-	public static int N, K;
+	static int N, K;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		K = sc.nextInt();
 		sc.close();
+
+		solve();
+	}
+
+	private static void solve() {
 		Queue<Integer> q = new LinkedList<>();
-		Queue<Integer> ans = new LinkedList<>();
-		for(int i = 1; i <= N; i++) {
+		for (int i = 1; i <= N; i++) {
 			q.offer(i);
 		}
-		
+
+		StringBuffer result = new StringBuffer("<");
 		int cnt = 1;
-		while(q.size() > 1) {
-			if(cnt == K) {
-				ans.offer(q.poll());
+		while (q.size() > 1) {
+			if (cnt == K) {
+				result.append(q.poll() + ", ");
 				cnt = 1;
 				continue;
 			}
 			q.offer(q.poll());
 			cnt++;
 		}
-		
-
-		
-		
-		System.out.println(ans.size());
-		System.out.print("<");
-		while(ans.size() > 1) {
-			System.out.print(ans.poll()+", ");
-		}
-		System.out.print(ans.poll());
-		System.out.print(">");
-
+		System.out.print(result.toString() + q.poll() + ">");
 	}
 
 }
