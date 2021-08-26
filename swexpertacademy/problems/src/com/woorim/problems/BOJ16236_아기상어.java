41ed0a47f5aa4f06a7d70e65e94a24bca7fc8b85
woorim1022 <woorim1022@naver.com>
2021-08-26 22:44:20 +0900

    Modify BOJ16236_아기상어.java

--- swexpertacademy/problems/src/com/woorim/problems/BOJ16236_아기상어.java
+++ swexpertacademy/problems/src/com/woorim/problems/BOJ16236_아기상어.java
@@ -1,25 +1,86 @@
 package com.woorim.problems;
-
+//처음에 아기 상어의 크기는 2이고, 
+//아기 상어는 1초에 상하좌우로 인접한 한 칸씩 이동한다.
+//자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없고,
+//나머지 칸은 모두 지나갈 수 있다.
+//자신의 크기보다 작은 물고기만 먹을 수 있다. 
+//크기가 같은 물고기는 먹을 수 없지만, 그 물고기가 있는 칸은 지나갈 수 있다.
+/*
+ 더 이상 먹을 수 있는 물고기가 공간에 없다면 아기 상어는 엄마 상어에게 도움을 요청한다.
+먹을 수 있는 물고기가 1마리라면, 그 물고기를 먹으러 간다.
+먹을 수 있는 물고기가 1마리보다 많다면, 거리가 가장 가까운 물고기를 먹으러 간다.
+거리는 아기 상어가 있는 칸에서 물고기가 있는 칸으로 이동할 때, 
+지나야하는 칸의 개수의 최솟값이다.
+거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기, 그러한 물고기가 여러마리라면, 
+가장 왼쪽에 있는 물고기를 먹는다.
+ */
+//자신의 크기와 같은 수의 물고기를 먹을 때 마다 크기가 1 증가한다. 
+//예를 들어, 크기가 2인 아기 상어는 물고기를 2마리 먹으면 크기가 3이 된다.
+//0: 빈 칸
+//1, 2, 3, 4, 5, 6: 칸에 있는 물고기의 크기
+//9: 아기 상어의 위치
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ16236_아기상어 {
+	static class Pos{
+		int r, c;
+		public Pos(int r, int c, int moveCnt) {
+			this.r = r;
+			this.c = c;
+		}
+	}
+	static int[] dx = {-1, 0, 0, 1};
+	static int[] dy = {0, -1, 1, 0};
 	static int N;	// 맵의 크기
 	static int[][] map;
 	static int[] shark, fish; // 상어정보, 먹을 물고기 정보
 	static int moveCnt;
 	static boolean[][] visited;
+	static int ans;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
-		int T = Integer.parseInt(br.readLine());
-		for (int t = 1; t <= T; t++) {
-			int ans = 0;
-			
-			System.out.print("#" + t + " " + ans);
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 9) {
+					shark = new int[] {i, j, 2};
+				}
+			}
 		}
+		solve();
+		System.out.println(ans);
 
+	}
+	private static void solve() {
+		Queue<Pos> q = new LinkedList<>();
+		q.offer(new Pos(shark[0], shark[1], moveCnt));
+		while(!q.isEmpty()) {
+			Pos pos = q.poll();
+			for(int i = 0; i < 4; i++) {
+				int nr = pos.r + dx[i];
+				int nc = pos.c + dy[i];
+				if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+				if(!visited[nr][nc] && map[nr][nc] <= shark[2]) {
+					// 방문하지 않았고 상어크기보다 작으면(0이거나 작은먹이면)
+					// 0이거나 map[nr][nc] == shark[2]
+						// 그냥 다음으로
+					// 상어크기보다 작은경우
+						// 냠 -> 현재위치 = 0
+						// 먹은 횟수 == 상어크기
+							// 상어크기++;
+							// 먹은횟수 초기화;
+						// 다음으로
+				}
+			}
+		}
 	}
 
 }
