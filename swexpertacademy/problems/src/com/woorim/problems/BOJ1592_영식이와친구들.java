b5b45507bd371eb8318ca4779e6763ed4029dac4
woorim1022 <woorim1022@naver.com>
2021-08-29 23:24:22 +0900

    Upload problems

---
+++ swexpertacademy/problems/src/com/woorim/problems/BOJ1592_영식이와친구들.java
@@ -0,0 +1,23 @@
+package com.woorim.problems;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1592_영식이와친구들 {
+	static int N, M, L;
+	static int answer;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		
+
+		
+		
+		System.out.println(answer);
+	}
+
+}
