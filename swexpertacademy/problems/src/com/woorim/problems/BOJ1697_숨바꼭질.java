e5a2c281fb0cfb700e0122a75c899ccc2b1d2c60
woorim1022 <woorim1022@naver.com>
2021-09-08 22:36:52 +0900

    Modift BOJ1697_숨바꼭질.java

--- swexpertacademy/problems/src/com/woorim/problems/BOJ1697_숨바꼭질.java
+++ swexpertacademy/problems/src/com/woorim/problems/BOJ1697_숨바꼭질.java
@@ -1,14 +1,22 @@
+//다익스트라 알고리즘
 package com.woorim.problems;
 
 import java.util.Scanner;
 
 public class BOJ1697_숨바꼭질 {
 	static int N, K;
+	static int answer;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		K = sc.nextInt();
-
+		
+		dijkstra(시작정점, 인접행렬, 시작정점에서의 거리);
+		
+		System.out.println(answer);
 	}
 
+	private static void dijkstra(int start, int ) {
+		
+	}
 }
